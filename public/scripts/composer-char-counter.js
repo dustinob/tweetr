c049bfcb44667528baaffcd44a99a660f8520e3d
Dustin OBrien <dcobrien82@gmail.com>
2017-01-25 02:29:17 +0000

    character counter functions and turns red when over 140 count

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+$(function() {
+  $(".new-tweet form").on("keyup", "textarea", function() {
+    var textArea = $(this);
+    var keyCount = (140 - (textArea.val().length));
+    if (keyCount > 0) {
+      textArea.siblings(".counter").addClass("counter").text(keyCount).css("color", "black");
+    }
+    else {
+      textArea.siblings(".counter").addClass("counter").text(keyCount).css("color", "red");
+    }
+  });
+});
